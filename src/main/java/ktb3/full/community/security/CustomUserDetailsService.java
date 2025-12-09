35a06b7ca19bb61d732a46e707371f818c06ea4d
CHAEGODA <chae_goda@naver.com>
2025-12-09 09:45:10 +0900

    feat: Spring Security 적용을 위한 로직 추가

---
+++ src/main/java/ktb3/full/community/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package ktb3.full.community.security;
+
+import ktb3.full.community.user.domain.User;
+import ktb3.full.community.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return new CustomUserDetails(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getRole()
+        );
+    }
+}
